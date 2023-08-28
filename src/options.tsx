5a9f6dc86b619ec678fe862490b3450ee6a7a29b
devlopersabbir <devlopersabbir@gmail.com>
2023-08-29 01:56:59 +0600

    Project setup done with tailwind css

--- src/options.tsx
+++ src/options.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Option from "./options/Option";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("options")!).render(
   <React.StrictMode>
