fad1ef4bbbbf2bccf6e982f41395c33db46180f0
devlopersabbir <devlopersabbir@gmail.com>
2023-09-20 03:01:42 +0600

    updated with content css

--- src/content.ts
+++ src/content.ts
@@ -1,19 +1,21 @@
 import browser from "webextension-polyfill";
 
 class Main {
-  public static facebook() {
-    const facebookElement: HTMLDivElement = document.getElementsByClassName(
-      "x1hc1fzr x1unhpq9 x6o7n8i"
-    )[0] as HTMLDivElement;
+  // public static facebook() {
+  //   const facebookElement: HTMLDivElement = document.getElementsByClassName(
+  //     "x1hc1fzr x1unhpq9 x6o7n8i"
+  //   )[0] as HTMLDivElement;
 
-    if (facebookElement) {
-      facebookElement.style.display = "none";
-    } else {
-      console.log("not found!");
-    }
-  }
+  //   if (facebookElement) {
+  //     facebookElement.style.display = "none";
+  //   } else {
+  //     console.log("not found!");
+  //   }
+  // }
 
   public static instagram() {}
 }
 
-Main.facebook();
+// Main.facebook();
+
+console.log("hello world");
